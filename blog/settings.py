58cbe83f902609198f818dfcee9732073efec343
= <=>
2016-11-05 20:02:28 +0000

    Settings

--- blog/settings.py
+++ blog/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blogdb',
         'USER': 'bloguser',
-        'PASSWORD': 'BlogCurso#456123',
+        'PASSWORD': '123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
